79e6267cf224c8ca827202d814271f4258a90e25
Ivan Zubok <chi_no@ukr.net>
2024-09-12 20:23:18 +0300

    feat: added recipe generator view

---
+++ src/app/modules/admin/recipes-generator/components/recipe-generator-view/recipe-generator-view.component.ts
@@ -0,0 +1,9 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+    selector: 'recipe-generator-view',
+    standalone: true,
+    templateUrl: './recipe-generator-view.component.html',
+    encapsulation: ViewEncapsulation.None,
+})
+export class RecipeGeneratorViewComponent {}
--- src/app/modules/admin/recipes-generator/recipes-generator.routes.ts
+++ src/app/modules/admin/recipes-generator/recipes-generator.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { RecipeGeneratorCreateComponent } from './components/recipe-generator-create/recipe-generator-create.component';
+import { RecipeGeneratorViewComponent } from './components/recipe-generator-view/recipe-generator-view.component';
 import { RecipesGeneratorComponent } from './recipes-generator.component';
 
 export default [
     {
         path: '',
         component: RecipesGeneratorComponent,
+    },
+    {
+        path: ':id',
+        component: RecipeGeneratorViewComponent,
     },
     {
         path: 'create',
